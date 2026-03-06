e967e87f9e572e717d637b7c8e75e13c92e110e1
sddiana <mosethvatit@gmail.com>
2026-03-06 13:19:38 +0700

    test card

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+Vue.component('card', {
+    data() {
+        return {
+            title: 'Тестовая карточка 1',
+            items: [
+                {name: "пункт 1", checked: false},
+                {name: "пункт 2", checked: false},
+                {name: "пункт 3", checked: false}
+            ]
+        }
+    },
+    template: `
+    <div class="card">
+        <h3>{{ title }}</h3>
+        <div v-for="(item, index) in items" :key="index">
+            <input type="checkbox" v-model="item.checked">
+            <span>{{ item.name }}</span>
+        </div>
+    </div>
+    `,
+})
+
 let app = new Vue({
-    el: '@app',
+    el: '#app',
 })
